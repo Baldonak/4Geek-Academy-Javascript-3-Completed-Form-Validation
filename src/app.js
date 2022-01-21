902dd4106258d7ea2d74059a68808c296967bd75
Baldonak <victormaynougomez@gmail.com>
2022-01-21 07:58:17 +0000

    Fix error in preventDefault

--- src/app.js
+++ src/app.js
@@ -6,8 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 //Validación---------------------------------------------------------------------------------------
 function CustomValidation() {
+  //Construir un objeto que contenga todos los mensajes de error y si aparecen o no <--------No implementado
+  /*
+
+  this.invalidities = {
+
+    User:{
+      This input needs to be at least 3 characters long : true,
+      Must only contain letters and numbers (no special characters): true,
+    }
+  }
+
+  */
+  ////Array que contiene los mensajes de error que van ocurriendo cada vez que se toca una tecla
   this.invalidities = [];
-  this.notInvalidities = true;
+  //Array que contiene los booleanos que nos dice si ocurre o no el error asociado
+  //notInvalidities[0] ->This input needs to be at least 3 characters long
+  this.notInvalidities = [false];
 }
 
 CustomValidation.prototype = {
@@ -31,12 +46,13 @@ CustomValidation.prototype = {
 
   //Función para hacer las comprobaciones:
   //  1-Añade mensaje de error al array "invalidities" si se cumple la condición
-  //  2-Elimina el mensaje de error del array si no se cumple <-----------(VICTOR) Imprementar
-  //  función
-  //  3-Cambia el boolean "notInvalidities" a false si alguna condición no se cumple
+  //  2-Elimina el mensaje de error del array si no se cumple función <---------Por Imprementar
+  //  3-Cambia el boolean "notInvalidities" a false si alguna condición no se cumple <---------Por Corregir
+  //  4-Cambia clase del mensaje de error del html a no valido o valido para que cambie en el
+  //  HTML
 
   checkValidity: function(input) {
-    // UserName
+    // UserName------------------------------------------------------------------------------------
     // Condition 1: This input needs to be at least 3 characters long
     if (input.value.length < 3) {
       this.addInvalidity("This input needs to be at least 3 characters long");
@@ -45,35 +61,39 @@ CustomValidation.prototype = {
       );
       element.classList.add("invalid");
       element.classList.remove("valid");
-      this.notInvalidities = false;
+      this.notInvalidities[0] = false;
     } else {
       var element = document.querySelector(
         'label[for="uname"] li:nth-child(1)'
       );
       element.classList.remove("invalid");
       element.classList.add("valid");
+      this.notInvalidities[0] = true;
     }
-    // Condition 2: Must only contain letters and numbers (no special characters)
-    if (input.value.match(/[^a-zA-Z0-9]/g)) {
-      this.addInvalidity(
-        "Must only contain letters and numbers (no special characters)"
-      );
-      var element = document.querySelector(
-        'label[for="uname"] li:nth-child(2)'
-      );
-      element.classList.add("invalid");
-      element.classList.remove("valid");
-      this.notInvalidities = false;
-    } else {
-      var element = document.querySelector(
-        'label[for="uname"] li:nth-child(2)'
-      );
-      element.classList.add("valid");
-      element.classList.remove("invalid");
-    }
+
+    console.log(`Validation: ${this.notInvalidities}`); //Test
+    // Condition 2: Must only contain letters and numbers (no special characters) <-------------No se esta evaluando
+    //     if (input.value.match(/[^a-zA-Z0-9]/g)) {
+    //       this.addInvalidity(
+    //         "Must only contain letters and numbers (no special characters)"
+    //       );
+    //       var element = document.querySelector(
+    //         'label[for="uname"] li:nth-child(2)'
+    //       );
+    //       element.classList.add("invalid");
+    //       element.classList.remove("valid");
+    //       this.notInvalidities[1] = false;
+    //     } else {
+    //       var element = document.querySelector(
+    //         'label[for="uname"] li:nth-child(2)'
+    //       );
+    //       element.classList.add("valid");
+    //       element.classList.remove("invalid");
+    //       this.notInvalidities[1] = true;
+    //     }
   }
 };
-//-------------------------------------
+//-------------------------------------------------------------------------------------------------
 
 var user_name_input = document.querySelector("#uname");
 user_name_input.CustomValidation = new CustomValidation();
@@ -82,29 +102,35 @@ user_name_input.CustomValidation = new CustomValidation();
 
 var notFormInvalidities =
   user_name_input.CustomValidation
-    .notInvalidities; /*&& cardNo_input.notInvalidities*/
+    .notInvalidities[0]; /*&& cardNo_input.notInvalidities*/
 
 //Validación de los campos del formulario
 user_name_input.addEventListener("keyup", function() {
   user_name_input.CustomValidation.checkValidity(this);
-  console.log(`Validation: ${notFormInvalidities}`);
-  console.log(user_name_input.CustomValidation.invalidities); //<---------------------(Porque no se hace visible?)
+  console.log(`Validation final: ${notFormInvalidities}`); //<---------------------(¿Por qué muestra siempre true?)
+  console.log(
+    `Mensajes de error en array: ${user_name_input.CustomValidation.getInvalidity()}`
+  );
 });
 
-//Crea evento para el form que detiene el envio del formulario
-//si notFormInvalidities es "false"
-
+//Para el envío del formulario si "notFormInvalidities" es "false"
 var form = document.querySelector("#form");
 form.addEventListener("submit", function(event) {
   if (!notFormInvalidities) {
     //--------------------------------------------------------
-    alert("Any error");
-    my_alert.classList.replace("display_none", "display_block");
+    alert("There are some errors. Cannot send the form");
     event.preventDefault();
+    event.stopPropagation();
   }
 });
 
-//-------------------------------------------------------------------------------------------------
+// form.addEventListener("submit", function(event) {
+//   if (true) {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     my_alert.classList.replace("display_none", "display_block");
+//   }
+// });
 
 // //Código de Bootstrap------------------------------------------------------------------------------
 // (function() {
